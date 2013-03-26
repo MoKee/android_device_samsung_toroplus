d1ddbdb45578f3da097d3071c627ed1d2ed4c74b
yun3195 <yun3195@gmail.com>
2013-03-26 20:45:10 +0800

    a new file has been created
    Change-Id: I8a434f210c88767e388ae4bc4e2be0161a1401ed

--- mokee.mk
+++ mokee.mk
@@ -6,20 +6,20 @@ TARGET_SCREEN_HEIGHT := 1280
 TARGET_SCREEN_WIDTH := 720
 
 # Inherit some common CM stuff.
-$(call inherit-product, vendor/cm/config/common_full_phone.mk)
+$(call inherit-product, vendor/mk/config/common_full_phone.mk)
 
 # Inherit CDMA common stuff
-$(call inherit-product, vendor/cm/config/cdma.mk)
+$(call inherit-product, vendor/mk/config/cdma.mk)
 
 # Inherit device configuration
 $(call inherit-product, device/samsung/toroplus/full_toroplus.mk)
 
 # Enhanced NFC
-$(call inherit-product, vendor/cm/config/nfc_enhanced.mk)
+$(call inherit-product, vendor/mk/config/nfc_enhanced.mk)
 
 ## Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := toroplus
-PRODUCT_NAME := cm_toroplus
+PRODUCT_NAME := mk_toroplus
 PRODUCT_BRAND := Google
 PRODUCT_MODEL := Galaxy Nexus
 PRODUCT_MANUFACTURER := Samsung
